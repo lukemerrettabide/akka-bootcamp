4de6563c72eebeeb9565eb62cd2a5b4ad00ec92f
Luke Merrett <luke.merrett@abide-financial.com>
2015-07-03 17:03:40 +0100

    Finished lesson 2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,8 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string StartCommand = "start";
+        
         private readonly IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -19,6 +21,73 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
+            if (message != null && message.Equals(StartCommand))
+            {
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
+            }
+
+            GetAndValidateInput();
+        }
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                // signal that the user needs to supply an input, as previously
+                // received input was blank
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                // shut down the entire actor system (allows the process to exit)
+                Context.System.Shutdown();
+            }
+            else
+            {
+                var valid = IsValid(message);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+
+                    // continue reading messages from console
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+        }
+
+        private static void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        /// <summary>
+        /// Validates <see cref="message"/>.
+        /// Currently says messages are valid if contain even number of characters.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static bool IsValid(string message)
+        {
+            var valid = message.Length % 2 == 0;
+            return valid;
+        }
+    }
+}
+
+/*
+
+
             var read = Console.ReadLine();
 
             _consoleWriterActor.Tell(read);
@@ -32,6 +101,4 @@ namespace WinTail
             }
 
             Self.Tell("bob");
-        }
-    }
-}
+*/
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -3,7 +3,6 @@
 
 namespace WinTail
 {
-    #region Program
     class Program
     {
         public static ActorSystem MyActorSystem;
@@ -11,8 +10,6 @@ namespace WinTail
         static void Main(string[] args)
         {
             MyActorSystem = ActorSystem.Create("MyActorSystem");
-
-            PrintInstructions();
 
             // time to make your first actors!
             //YOU NEED TO FILL IN HERE
@@ -22,27 +19,10 @@ namespace WinTail
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create<ConsoleWriterActor>());
             var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
-            consoleReaderActor.Tell("bob");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.AwaitTermination();
         }
-
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
-    #endregion
 }
